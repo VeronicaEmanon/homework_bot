b981ed00f7181d65bb19d605e019f581fc9b69cc
Никитина Вероника <descucci@mail.ru>
2022-09-26 18:03:55 +0900

    S7

--- homework.py
+++ homework.py
@@ -11,7 +11,7 @@ PRACTICUM_TOKEN = os.getenv("PRACTICUM_TOKEN")
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
 TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
 
-RETRY_TIME: int = 60 *60 * 10
+RETRY_TIME: int = 60 * 60 * 10
 ENDPOINT = "https://practicum.yandex.ru/api/user_api/homework_statuses/"
 HEADERS = {"Authorization": f"OAuth {PRACTICUM_TOKEN}"}
 
